9b634b53172890c3a922493a450d2795985291e5
yoonthegoon <71526721+yoonthegoon@users.noreply.github.com>
2025-11-07 16:45:43 -0500

    use pathlib

--- everybody_codes/utils.py
+++ everybody_codes/utils.py
@@ -1,10 +1,10 @@
-from os.path import abspath, dirname, join
+from pathlib import Path
 
-ROOT_DIR = dirname(dirname(abspath(__file__)))
+ROOT_DIR = Path(__file__).parents[1]
 
 
 def get_notes(event: int, quest: int, part: int) -> str:
-    file = join(ROOT_DIR, f"notes/everybody_codes_e{event}_q{quest:02}_p{part}.txt")
+    file = ROOT_DIR / f"notes/everybody_codes_e{event}_q{quest:02}_p{part}.txt"
     with open(file) as f:
         notes = f.read()
     return notes
