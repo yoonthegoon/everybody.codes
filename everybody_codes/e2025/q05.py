2c74b85df3b3241cf4252a6fd209b197496f3f2b
yoonthegoon <71526721+yoonthegoon@users.noreply.github.com>
2025-11-08 01:56:07 -0500

    refactor Segment and Sword classes

--- everybody_codes/e2025/q05.py
+++ everybody_codes/e2025/q05.py
@@ -2,86 +2,81 @@ from typing import Optional
 
 
 class Segment:
-    def __init__(self, value: int):
+    value: int
+    left: Optional[int]
+    right: Optional[int]
+    next: Optional[Segment]
+
+    def __init__(self, value: int) -> None:
         self.value = value
         self.left = None
         self.right = None
-        self.bottom = None
+        self.next = None
 
-    def __lt__(self, other: Segment) -> Optional[bool]:
-        if self.number < other.number:
-            return True
-        if self.number > other.number:
-            return False
-        if self.bottom is None or other.bottom is None:
-            return None
-        return self.bottom < other.bottom
-
-    def _construct(self, value: int):
-        if self.left is None and value < self.value:
-            self.left = value
-        elif self.right is None and value > self.value:
-            self.right = value
-        elif self.bottom is None:
-            self.bottom = Segment(value)
+    def _construct(self, number: int) -> None:
+        if number < self.value and self.left is None:
+            self.left = number
+        elif number > self.value and self.right is None:
+            self.right = number
+        elif self.next is None:
+            self.next = Segment(number)
         else:
-            self.bottom._construct(value)
+            self.next._construct(number)
 
-    def construct(self, values: list[int]):
-        for value in values:
-            self._construct(value)
-
-    @property
-    def quality(self) -> str:
-        if self.bottom is None:
-            return str(self.value)
-        return str(self.value) + self.bottom.quality
-
-    @property
-    def number(self) -> int:
-        number_str = ""
-        if self.left is not None:
-            number_str += str(self.left)
-        number_str += str(self.value)
-        if self.right is not None:
-            number_str += str(self.right)
-        return int(number_str)
-
-
-class Sword:
-    def __init__(self, note: str):
-        self.note = note
-
-        identifier_str, numbers_str = note.split(":")
-        numbers = list(map(int, numbers_str.split(",")))
-
-        self.identifier = int(identifier_str)
-        self.spine = Segment(numbers[0])
-        self.spine.construct(numbers[1:])
-
-    def __lt__(self, other: Sword) -> bool:
-        if self.quality < other.quality:
-            return True
-        if self.quality > other.quality:
-            return False
-        segment_lt = self.spine < other.spine
-        if segment_lt is None:
-            return self.identifier < other.identifier
-        return segment_lt
+    def construct(self, numbers: list[int]) -> None:
+        for number in numbers:
+            self._construct(number)
 
     @property
     def quality(self) -> int:
-        return int(self.spine.quality)
+        if self.next is None:
+            return self.value
+        return int(str(self.value) + str(self.next.quality))
+
+    @property
+    def number(self) -> int:
+        buff = ""
+        if self.left is not None:
+            buff += str(self.left)
+        buff += str(self.value)
+        if self.right is not None:
+            buff += str(self.right)
+        return int(buff)
+
+
+class Sword:
+    def __init__(self, note: str) -> None:
+        identifier_str, numbers_str = note.split(":")
+        identifier = int(identifier_str)
+        numbers = list(map(int, numbers_str.split(",")))
+
+        self.identifier = identifier
+        self.fishbone = Segment(numbers[0])
+        self.fishbone.construct(numbers[1:])
+
+    def __lt__(self, other: Sword) -> bool:
+        self_segment, other_segment = self.fishbone, other.fishbone
+        if self_segment.quality < other_segment.quality:
+            return True
+        if self_segment.quality > other_segment.quality:
+            return False
+        while (self_segment is not None) or (other_segment is not None):
+            if self_segment.number < other_segment.number:
+                return True
+            if self_segment.number > other_segment.number:
+                return False
+            self_segment, other_segment = self_segment.next, other_segment.next
+        return self.identifier < other.identifier
 
 
 def p1(notes: str) -> int:
     sword = Sword(notes)
-    return sword.quality
+    return sword.fishbone.quality
 
 
 def p2(notes: str) -> int:
     swords = [Sword(note) for note in notes.split()]
-    return max(swords).quality - min(swords).quality
+    return max(swords).fishbone.quality - min(swords).fishbone.quality
 
 
 def p3(notes: str) -> int:
